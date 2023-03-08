099fd99419e988d0374494f0b63a7def2d2d23c8
zunohan <pnlan1406@gmail.com>
2023-03-08 17:28:59 +0700

    first cocos game

---
+++ assets/scripts/Login.ts
@@ -0,0 +1,13 @@
+import { _decorator, Component, Node } from "cc";
+import axios from "axios";
+
+const { ccclass, property } = _decorator;
+
+@ccclass("Login")
+export class Login extends Component {
+  private state: any;
+
+  start() {}
+
+  update(deltaTime: number) {}
+}
